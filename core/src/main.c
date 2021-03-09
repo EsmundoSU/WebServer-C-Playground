d8d0b5b4acde2fb455546072bd9af2d780a17916
AdamPelcSU <adam.pelc.su@gmail.com>
2021-03-09 16:23:40 +0100

    build: CMakeFiles project refactoring.
    Split CMakeFiles.txt to smaller parts.

--- core/src/main.c
+++ core/src/main.c
@@ -2,8 +2,9 @@
 #include "server.h"
 #include "logger.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+	log_debug("Launch loc:\t%s", argv[0]);
 	// Set global app logger.
 	log_enable(LOG_ENABLE);
 	log_set_level(LOG_DEBUG);
